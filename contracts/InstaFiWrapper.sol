808529d07e35a1db58335d87a3b0c39d33d73543
Praveen Kumar Pendyala <praveendath92@gmail.com>
2022-07-02 00:24:06 +0100

    Changed structure to pass in function selector and args separately

--- contracts/InstaFiWrapper.sol
+++ contracts/InstaFiWrapper.sol
@@ -5,24 +5,37 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract InstaFiWrapper {
 
+    struct CallUnit {
+        address contractAddress;
+        bytes4 functionSelector;
+        bytes functionArguments;
+        uint256 value;
+    }
+
     constructor() { }
 
     /**
      * Call the given contracts with the given callData
      */
-    function executeExternal(
-        address[] calldata contracts, 
-        bytes[] calldata methodData, 
-        uint256[] calldata methodValues
-    ) public payable {
-        require(contracts.length == methodData.length, "CONTRACTS_METHODDATA_LENGTH_MISSMATCH");
-        require(contracts.length == methodValues.length, "CONTRACTS_METHODVALUES_LENGTH_MISSMATCH");
-
+    function executeExternal(CallUnit[] memory callUnits) public payable {
         bool callExecuted = false;
-        for (uint256 i = 0; i < contracts.length; i++) {
-            (callExecuted, ) = contracts[i].call{ value: methodValues[i] }(methodData[i]);
+        bytes memory callData;
+
+        for (uint256 i = 0; i < callUnits.length; i++) {
+            callData = bytes.concat(callUnits[i].functionSelector, callUnits[i].functionArguments);
+
+            (callExecuted, ) = callUnits[i].contractAddress.call{ value: callUnits[i].value }(callData);
             require(callExecuted, string(abi.encodePacked("EXTERNAL_CALL_FAILED in call #", Strings.toString(i))));
+
+            emit CallExecuted(
+                callUnits[i].contractAddress, 
+                callUnits[i].functionSelector, 
+                callUnits[i].functionArguments, 
+                callUnits[i].value
+            );
         }
     }
+
+    event CallExecuted(address indexed contractAddress, bytes4 indexed functionSelector, bytes functionArguments, uint256 value);
 
 }
